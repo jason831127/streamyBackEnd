c940484a5d667b2218202b859332ad9e6bb0a559
Unknown <jason831127@gmail.com>
2019-02-05 23:47:04 +0800

    fix: 修改db設定

--- build/docker/db/script/001.init.sql
+++ build/docker/db/script/001.init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS mydb;
 USE mydb;
-CREATE USER IF NOT EXISTS db_user@127.0.0.1 IDENTIFIED BY 'db_user';
-GRANT ALL PRIVILEGES ON mydb.* TO 'db_user'@'127.0.0.1';
+CREATE USER IF NOT EXISTS db_user@localhost IDENTIFIED BY 'db_user';
+GRANT ALL PRIVILEGES ON mydb.* TO 'db_user'@'localhost';
